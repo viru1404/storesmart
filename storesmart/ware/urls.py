63e7e615f26d9c9b00172b63456ec2086005f2c7
Sameer87 <direct2sameer@gmail.com>
2016-07-16 12:13:19 +0530

    update again

--- storesmart/ware/urls.py
+++ storesmart/ware/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url,include
 from .views import home
 urlpatterns = [
     url(r'^$', home,name="home"),
+    url(r'^account/register/$',register,name='register'),
+    url(r'^account/login/$',logintoit,name='login'),
+    url(r'^account/logout/$',logout1,name='logout'),
 ]
